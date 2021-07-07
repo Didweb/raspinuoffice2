f3ec28a288d12db95c05e6c3f8bf9a1cea34cff5
Didweb <info@did-web.com>
2021-07-07 10:11:20 +0200

    refractor angular

--- angular/app/components/default.component.ts
+++
@@ -1,9 +0,0 @@
-
-import {Component, OnInit} from '@angular/core';
-
-@Component({
-    selector: 'default',
-    template: '<h1>Compenent por defecto</h1>'
-})
-
-export class DefaultComponent { }
--- angular/app/components/register.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component, OnInit} from '@angular/core';
-
-@Component({
-    selector: 'register',
-    template: '<h1>Formulario register</h1>'
-})
-
-export class RegisterComponent { }
